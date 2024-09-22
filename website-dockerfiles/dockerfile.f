41b1cdedebd9ad4e7c3262e94b176f447c19e9ff
seunayolu <awscloudpros@gmail.com>
2024-09-22 16:33:37 +0100

    moodle lms dockerfile updated

--- website-dockerfiles/dockerfile.f
+++ website-dockerfiles/dockerfile.f
@@ -1,17 +1,18 @@
 # Use the official PHP 8.3 image as a base
 FROM php:8.3-apache
 
-# Install necessary extensions and download/unzip template in a single layer
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends unzip wget git && \
-    mkdir -p /tmp/webfiles && \
-    cd /tmp/webfiles && \
-    wget https://www.tooplate.com/zip-templates/2133_moso_interior.zip && \
-    unzip 2133_moso_interior.zip && \
-    cp -r 2133_moso_interior/* /var/www/html/ && \
-    rm -rf /tmp/webfiles && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+# Install necessary extensions
+RUN apt update && apt install -y unzip wget git
+
+# Creating Temp Directory
+RUN mkdir -p /tmp/webfiles && cd /tmp/webfiles
+RUN wget https://www.tooplate.com/zip-templates/2133_moso_interior.zip
+RUN unzip 2133_moso_interior.zip
+RUN cp -r 2133_moso_interior/* /var/www/html/
+RUN cd ~
+
+# Clean Up
+RUN rm -rf /tmp/webfiles
 
 # Expose port 80
 EXPOSE 80
